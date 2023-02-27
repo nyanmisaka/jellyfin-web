db7969e5cdb42807fd1e8628bf83b33a77c8ce97
nyanmisaka <nst799610810@gmail.com>
2023-02-27 20:21:09 +0800

    Disable progressive transcoding

--- src/scripts/browserDeviceProfile.js
+++ src/scripts/browserDeviceProfile.js
@@ -771,6 +771,8 @@ import browser from './browser';
         }
 
         // Progressive mp4 transcoding
+        // Disable for now since it causes audio and video out of sync.
+/*
         if (mp4VideoCodecs.length && videoAudioCodecs.length) {
             profile.TranscodingProfiles.push({
                 Container: 'mp4',
@@ -791,7 +793,7 @@ import browser from './browser';
             Context: 'Static',
             Protocol: 'http'
         });
-
+*/
         profile.ContainerProfiles = [];
 
         profile.CodecProfiles = [];
