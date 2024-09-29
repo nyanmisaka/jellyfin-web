c086cf4a9dd9506aaa45fd9c2ca4ef4298097fd4
nyanmisaka <nst799610810@gmail.com>
2024-09-29 23:00:12 +0800

    Optimize the default font for libass-wasm
    Signed-off-by: nyanmisaka <nst799610810@gmail.com>

--- src/plugins/htmlVideoPlayer/plugin.js
+++ src/plugins/htmlVideoPlayer/plugin.js
@@ -1273,7 +1273,7 @@ export class HtmlVideoPlayer {
             api_key: apiClient.accessToken()
         });
         const htmlVideoPlayer = this;
-        import('@jellyfin/libass-wasm').then(({ default: SubtitlesOctopus }) => {
+        import('libass-wasm').then(({ default: SubtitlesOctopus }) => {
             const options = {
                 video: videoElement,
                 subUrl: getTextTrackUrl(track, item),
